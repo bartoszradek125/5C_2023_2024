351688f88a2dd3624e3d2b0977500ee9bfd4d845
bartoszradek125 <19c.bartosz.radek@ezse.pl>
2023-11-08 14:32:26 +0100

    1

---
+++ 08_11_23/zad3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Obliczanie wartosci bezwzglednej</title>
+</head>
+<body>
+    <?php
+    $liczba = -10;
+    $bezwzgledna = abs($liczba);
+
+    echo "Wartosc bezwzględna z liczby {$liczba} wynosi: " . $bezwzgledna;
+    ?>
+</body>
+</html>
