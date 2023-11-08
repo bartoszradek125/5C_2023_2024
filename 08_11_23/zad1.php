7514c28885fceeb71c16359bfaf9efb059a3d609
bartoszradek125 <19c.bartosz.radek@ezse.pl>
2023-11-08 14:25:11 +0100

    1

--- 08_11_23/zad1.php
+++ 08_11_23/zad1.php
@@ -1,1 +1,17 @@
-cos
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Porownanie zmiennych</title>
+</head>
+<body>
+    <?php
+    $zmienna1=15;
+    $zmienna2=10;
+    if($zmienna1>$zmienna2) {
+        echo "Wieksza wartosc to: " .$zmienna1;
+    } else {
+        echo "Wieksza wartosc to: " .$zmienna2;
+    }
+    ?>
+</body>
+</html>
