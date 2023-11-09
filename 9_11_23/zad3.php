c9c1860152b3d3312c49051b9b6528f6faf95a5c
bartoszradek125 <19c.bartosz.radek@ezse.pl>
2023-11-09 14:15:19 +0100

    1

---
+++ 9_11_23/zad3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liczby kolejne PHP2</title>
+</head>
+<body>
+    <?php
+    for ($i = 100; $i <=120; $i += 5) {
+        echo "Liczba podzielna przez 5: $i";
+    }
+    ?>
+</body>
+</html>
