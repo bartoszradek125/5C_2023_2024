ea2ac482385e59ca727a4b3911d1a44d29540e24
bartoszradek125 <19c.bartosz.radek@ezse.pl>
2023-11-09 14:44:33 +0100

    1

---
+++ 9_11_23/zad5.php
@@ -0,0 +1,35 @@
+!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Algorytm</title>
+</head>
+<body>
+    <?php
+    $function = function($x) {
+        return $x * $x;
+    }
+    $start = 0;
+    $end = 10;
+    $precision = 0.001;
+    $result = null;
+    $interations = 0;
+    while (($end - $start) > $precision) {
+        $midpoint = ($start + $end) /2;
+        $fMidpoint = $function($midpoint);
+
+        if ($fMidpoint == 0.0) {
+            $result = $midpoint;
+            break;
+        } elseif ($fMidpoint * $function($start) < 0){
+            $end = $midpoint;
+        } else {
+            $start = $midpoint;
+        }
+        $interations++;
+    }
+    echo "Wynik: $result, Iteracje: $iterations";
+    ?>
+</body>
+</html>
