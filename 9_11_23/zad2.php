d7878f9d6b2bcefcf7d9f2adf43a8fdd7e68fd19
bartoszradek125 <19c.bartosz.radek@ezse.pl>
2023-11-09 14:08:42 +0100

    1

---
+++ 9_11_23/zad2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liczby kolejne PHP</title>
+</head>
+<body>
+    <?php
+    for ($i = 100; $i >=91; $i--) {
+        echo "Liczba: $i";
+    }
+</body>
+</html>
